7ffb40830baa4f4639b26e6a241837b422bbede3
Ostap Haidar <ostaphaidar@gmail.com>
2026-06-08 14:54:24 +0300

    Implement IRenderer interface

--- include/ui.h
+++ include/ui.h
@@ -3,14 +3,22 @@
 #include <ncurses.h>
 #include <vector>
 
-class NcursesRenderer {
+class IRenderer {
+public:
+    virtual ~IRenderer() = default;
+    
+    virtual void draw(std::vector<ProcessInfo>& procs) = 0;
+    virtual void handle_input(int proc_count) = 0;
+};
+
+class NcursesRenderer : public IRenderer {
 public:
     NcursesRenderer();
     
-    void draw(std::vector<ProcessInfo>& procs);
-    void handle_input(int proc_count);
+    void draw(std::vector<ProcessInfo>& procs) override;
+    void handle_input(int proc_count) override;
 
-    ~NcursesRenderer();
+    ~NcursesRenderer() override;
 
 private:
     int scroll_offset_ = 0;
