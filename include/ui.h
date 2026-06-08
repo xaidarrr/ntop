3e52a821b458a4e0025da431affacafb3838da6a
Ostap Haidar <ostaphaidar@gmail.com>
2026-06-08 13:47:50 +0300

    Add NcursesRenderer constructor & destructor declaration to header file

--- include/ui.h
+++ include/ui.h
@@ -5,8 +5,12 @@
 
 class NcursesRenderer {
 public:
+    NcursesRenderer();
+    
     void draw(std::vector<ProcessInfo>& procs);
     void handle_input(int proc_count);
+
+    ~NcursesRenderer();
 
 private:
     int scroll_offset_ = 0;
